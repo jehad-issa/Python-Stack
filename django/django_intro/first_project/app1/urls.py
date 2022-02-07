b803b260470a1c4d532a6efefe90896d0ff863f2
jehad-issa <96985283+jehad-issa@users.noreply.github.com>
2022-02-07 14:25:25 +0200

    first_django_project

---
+++ django/django_intro/first_project/app1/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path     
+from . import views
+urlpatterns = [
+    path('blogs', views.index),
+    path('', views.root),
+    path('blogs/new', views.new),
+    path('blogs/create',views.create),
+    path('blogs/<number>',views.show),
+    path('blogs/<number>/edit',views.edit),
+    path('blogs/<number>/delet',views.destroy)
+    
+]
---
+++ django/django_intro/first_project/app1/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, HttpResponse,redirect
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def root(request):
+    return redirect('/blogs')
+
+def new(request):
+    return    HttpResponse("placeholder to later display a list of all blogs")
+
+def create(request):
+    return    redirect ("/")
+
+def show(request,number):
+    return    HttpResponse("placeholder to display blog number"+str(number))
+
+def edit(request,number):
+    return    HttpResponse("placeholder to display blog number"+str(number))   
+
+def destroy(request,number):
+    return redirect("/blogs")     
+
+
+
